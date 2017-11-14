040f2bd7b033395132b5394c28cb5892864bce88
tatsy <tatsy.mail@gmail.com>
2017-11-14 22:54:40 +0900

    Minor update.

---
+++ src/core/transform.go
@@ -0,0 +1,178 @@
+package core
+
+import (
+	"math"
+)
+
+// Transform is a 4x4 transformation matrix.
+type Transform struct {
+	mat [16]Float // Matrix entries
+}
+
+// NewTransform returns a pointer of the Transform.
+func NewTransform(mat [16]Float) *Transform {
+	t := new(Transform)
+	t.mat = mat
+	return t
+}
+
+// NewTransformWithElements returns a pointer of the Transform,
+// which has specified elements as its entries.
+func NewTransformWithElements(
+	m00, m01, m02, m03 Float,
+	m10, m11, m12, m13 Float,
+	m20, m21, m22, m23 Float,
+	m30, m31, m32, m33 Float) *Transform {
+	t := new(Transform)
+	t.mat = [...]Float{
+		m00, m01, m02, m03,
+		m10, m11, m12, m13,
+		m20, m21, m22, m23,
+		m30, m31, m32, m33,
+	}
+	return t
+}
+
+// NewScale returns a scaling Transform.
+func NewScale(sx, sy, sz Float) *Transform {
+	return NewTransformWithElements(
+		sx, 0.0, 0.0, 0.0,
+		0.0, sy, 0.0, 0.0,
+		0.0, 0.0, sz, 0.0,
+		0.0, 0.0, 0.0, 1.0)
+}
+
+// NewLookAt returns a pointer of the "lookAt" Transform.
+func NewLookAt(origin, target, up *Vector3d) *Transform {
+	m03 := origin.X
+	m13 := origin.Y
+	m23 := origin.Z
+	m33 := 1.0
+
+	dir := target.Subtract(origin).Normalized()
+	left := up.Cross(dir).Normalized()
+	newUp := dir.Cross(left)
+
+	m00 := left.X
+	m10 := left.Y
+	m20 := left.Z
+	m30 := 0.0
+	m01 := newUp.X
+	m11 := newUp.Y
+	m21 := newUp.Z
+	m31 := 0.0
+	m02 := dir.X
+	m12 := dir.Y
+	m22 := dir.Z
+	m32 := 0.0
+
+	return NewTransformWithElements(
+		m00, m01, m02, m03,
+		m10, m11, m12, m13,
+		m20, m21, m22, m23,
+		m30, m31, m32, m33,
+	)
+}
+
+// NewPerspective returns a pointer of the perspective Transform.
+func NewPerspective(fov, aspect, near, far Float) *Transform {
+	pers := [...]Float{
+		1.0 / aspect, 0.0, 0.0, 0.0,
+		0.0, 1.0, 0.0, 0.0,
+		0.0, 0.0, far / (far - near), -far * near / (far - near),
+		0.0, 0.0, 1.0, 0.0,
+	}
+	invTanHalf := 1.0 / math.Tan(fov*0.5)
+	return NewScale(invTanHalf, invTanHalf, 1.0).Multiply(NewTransform(pers))
+}
+
+// Multiply computes the multiplications of Transforms.
+func (t1 *Transform) Multiply(t2 *Transform) *Transform {
+	ret := new(Transform)
+	for i := 0; i < 4; i++ {
+		for j := 0; j < 4; j++ {
+			ret.mat[i*4+j] = 0.0
+			for k := 0; k < 4; k++ {
+				ret.mat[i*4+j] += t1.mat[i*4+k] * t2.mat[k*4+j]
+			}
+		}
+	}
+	return ret
+}
+
+// Inverted returns a inverted Transform.
+func (t *Transform) Inverted() *Transform {
+	indxc := make([]int, 4)
+	indxr := make([]int, 4)
+	ipiv := [4]int{0, 0, 0, 0}
+
+	mInv := [16]Float{}
+	for i := 0; i < 16; i++ {
+		mInv[i] = t.mat[i]
+	}
+
+	for i := 0; i < 4; i++ {
+		irow, icol := 0, 0
+		big := 0.0
+
+		// Choose pivot
+		for j := 0; j < 4; j++ {
+			if ipiv[j] != 1 {
+				for k := 0; k < 4; k++ {
+					if ipiv[k] == 0 {
+						if math.Abs(mInv[j*4+k]) >= big {
+							big = math.Abs(mInv[j*4+k])
+							irow = j
+							icol = k
+						}
+					} else if ipiv[k] > 1 {
+						panic("Singular matrix cannot be inverted")
+					}
+				}
+			}
+		}
+		ipiv[icol]++
+
+		// Swap pivot row
+		if irow != icol {
+			for k := 0; k < 4; k++ {
+				mInv[irow*4+k], mInv[icol*4+k] = mInv[icol*4+k], mInv[irow*4+k]
+			}
+		}
+
+		indxr[i] = irow
+		indxc[i] = icol
+		if math.Abs(mInv[icol*4+icol]) < Eps {
+			panic("Singular matrix cannot be inverted")
+		}
+
+		pinv := 1.0 / mInv[icol*4+icol]
+		mInv[icol*4+icol] = 1.0
+		for j := 0; j < 4; j++ {
+			mInv[icol*4+j] *= pinv
+		}
+
+		// Subtract diagonal value from the other rows
+		for j := 0; j < 4; j++ {
+			if j != icol {
+				save := mInv[j*4+icol]
+				mInv[j*4+icol] = 0.0
+				for k := 0; k < 4; k++ {
+					mInv[j*4+k] -= mInv[icol*4+k] * save
+				}
+			}
+		}
+	}
+
+	// Swap columins to reset permutations
+	for j := 3; j >= 0; j-- {
+		if indxr[j] != indxc[j] {
+			for k := 0; k < 4; k++ {
+				mInv[k*4+indxr[j]], mInv[k*4+indxc[j]] =
+					mInv[k*4+indxc[j]], mInv[k*4+indxr[j]]
+			}
+		}
+	}
+
+	return NewTransform(mInv)
+}
